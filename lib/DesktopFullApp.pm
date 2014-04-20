d6c19ed161b315f889a03c6eda3d12ff383dd80a
Nicky Ayoub <nicky.ayoub@gmail.com>
2014-04-20 09:51:02 -0700

    Replicated functionality of the lite app

--- lib/DesktopFullApp.pm
+++ lib/DesktopFullApp.pm
@@ -1,18 +1,52 @@
 package DesktopFullApp;
+use Carp;
 use Mojo::Base 'Mojolicious';
+use DBI;
 
 # This method will run once at server start
 sub startup {
   my $self = shift;
 
+    # connect to database
+    use DBI;
+    my $dbh = DBI->connect( "dbi:SQLite:database.db", "", "" )
+      or croak "Could not connect: $!";
+
+    # shortcut for use in template
+   $self->helper( db => sub { $dbh });
+
   # Documentation browser under "/perldoc"
   $self->plugin('PODRenderer');
+
+  my $config = $self->plugin('Config');
 
   # Router
   my $r = $self->routes;
 
   # Normal route to controller
   $r->get('/')->to('example#welcome');
+  $r->get('/kill') -> to('example#kill');
+
+
+    # setup route which receives data and returns to /
+  $r->post('/insert') -> to('example#insert');
+
+  my $insert;
+
+  while (1) {    # Repeat forever until disk space is available.
+
+        # create insert statement
+        $insert = eval { $dbh->prepare('INSERT INTO people VALUES (?,?)') };
+
+        # break out of loop if statement prepared
+        last if $insert;
+
+       # if statement didn't prepare, assume its because the table doesn't exist
+        warn "Creating table 'people'\n";
+        $dbh->do('CREATE TABLE people (name varchar(255), age int);');
+    }
+
+    $self->app->secrets(["Don't tell anyone"]);
 }
 
 1;
--- lib/DesktopFullApp/Example.pm
+++ lib/DesktopFullApp/Example.pm
@@ -1,12 +1,32 @@
 package DesktopFullApp::Example;
 use Mojo::Base 'Mojolicious::Controller';
-
+use Data::Dumper;
 # This action will render a template
 sub welcome {
   my $self = shift;
 
   # Render template "example/welcome.html.ep" with message
   $self->render(msg => 'Welcome to the Mojolicious real-time web framework!');
+}
+
+sub kill {
+  my $self = shift;  
+  $self->res->code(301);
+  my $c = $self->config;
+  my $name = $c->{'name'};
+  $self->redirect_to('http://www.google.com');
+  $self->app->log->debug("Goodbye, $name.");
+  $self->tx->on( finish => sub {exit} );
+}
+
+sub insert {
+    my $self = shift;
+    my $user = $self->param('name');
+    my $age  = $self->param('age');
+    my $dbh= $self->db;
+    my $insert = eval { $dbh->prepare('INSERT INTO people VALUES (?,?)') };
+    $insert->execute( $user, $age );
+    $self->redirect_to('/');
 }
 
 1;
